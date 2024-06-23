f18e88491d9c8b51cf1254f0fa013310841b397e
MatCat960 <mattia.catellani@unimore.it>
2024-06-23 14:27:09 +0000

    add target node and rviz visualizer

---
+++ src/target_node.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import math
+
+import rospy
+import tf
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+
+VMAX = 0.25
+
+class Target():
+    def __init__(self):
+        rospy.init_node("target_node")
+
+        self.width = rospy.get_param("AREA_W", 20.0)
+        self.id = rospy.get_param("TARGET_ID", 0)
+        self.dt = rospy.get_param("dt", 0.2)
+
+        self.timer = rospy.Timer(rospy.Duration(self.dt), self.timer_callback)
+        self.odomPub = rospy.Publisher("odom", Odometry, queue_size=1)
+        
+        self.odom_msg = Odometry()
+        self.odom_msg.header.frame_id = "odom"
+        self.odom_msg.child_frame_id = f"target_{self.id}"
+
+        # start in origin with random theta
+        self.target = np.zeros(3,)
+        self.target[2] = 2*math.pi*np.random.rand()
+
+    def timer_callback(self, e):
+        th = self.target[2]
+        new_x = self.target[0] + VMAX * np.cos(th)*self.dt
+        new_y = self.target[1] + VMAX * np.sin(th)*self.dt
+
+        # check if outside
+        if new_x < -0.5*self.width + 1:
+            new_x = -0.5*self.width + 1
+            th = -th - 0.5*np.pi + np.pi*np.random.rand()
+        elif new_x > 0.5*self.width - 1:
+            new_x = 0.5*self.width - 1
+            th = -th - 0.5*np.pi + np.pi*np.random.rand() 
+        
+        if new_y < -0.5*self.width + 1:
+            new_y = -0.5*self.width + 1
+            th = -th - 0.5*np.pi + np.pi*np.random.rand()
+
+        elif new_y > 0.5*self.width - 1:
+            new_y = 0.5*self.width - 1
+            th = -th - 0.5*np.pi + np.pi*np.random.rand()
+
+        # update target
+        self.target = np.array([new_x, new_y, th])
+
+        # publish ros msg
+        quaternion = tf.transformations.quaternion_from_euler(0.0, 0.0, th)
+        self.odom_msg.pose.pose.position.x = self.target[0]
+        self.odom_msg.pose.pose.position.y = self.target[1]
+        self.odom_msg.pose.pose.orientation.x = quaternion[0]
+        self.odom_msg.pose.pose.orientation.y = quaternion[1]
+        self.odom_msg.pose.pose.orientation.z = quaternion[2]
+        self.odom_msg.pose.pose.orientation.w = quaternion[3]
+        self.odom_msg.header.stamp = rospy.Time.now()
+        self.odomPub.publish(self.odom_msg)
+
+def main(args=None):
+    targetNode = Target()    
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+    
---
+++ src/viz_node.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import math
+
+import rospy
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+
+rospy.init_node("viz_node")
+
+width = rospy.get_param("AREA_W", 20.0)
+
+# timer = rospy.Timer(rospy.Duration(0.1), self.timer_callback)
+bound_pub = rospy.Publisher("boundaries", Marker, queue_size=1)
+
+bound_msg = Marker()
+bound_msg.header.frame_id = "odom"
+bound_msg.id = 0
+bound_msg.type = 4     # LineStrip
+bound_msg.action = Marker.ADD
+
+# color
+bound_msg.color.r = 1.0
+bound_msg.color.a = 1.0
+
+bound_msg.scale.x = 0.1
+
+# position
+square_points = [
+        Point(-0.5*width, -0.5*width, 0.0),
+        Point(0.5*width, -0.5*width, 0.0),
+        Point(0.5*width, 0.5*width, 0.0),
+        Point(-0.5*width, 0.5*width, 0.0),
+        Point(-0.5*width, -0.5*width, 0.0) # Closing the loop
+    ]
+
+bound_msg.points.extend(square_points)
+
+while not rospy.is_shutdown():
+    bound_pub.publish(bound_msg)
+
+        
+
+
+
+
+    
