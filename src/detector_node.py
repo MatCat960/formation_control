fix times broadcasting

src/detector_node.py
@@ -9,8 +9,9 @@ import math
 
 # ROS stuff
 import rospy
-from geometry_msgs.msg import Point, Twist, Pose
+from geometry_msgs.msg import Point, Twist, Pose, PoseStamped
 from nav_msgs.msg import Odometry, OccupancyGrid
+from std_msgs.msg import Float32, Int32MultiArray
 from tf.transformations import euler_from_quaternion
 import random
 
@@ -71,13 +72,14 @@ class Detector():
     self.FOV_RAD = self.FOV_DEG * math.pi / 180.0
     self.ROBOT_RANGE = rospy.get_param("ROBOT_RANGE", 3.0)
     self.dt = rospy.get_param("dt", 0.2)
-    self.GRID_SIZE = 100
+    self.GRID_SIZE = rospy.get_param("GRID_SIZE", 50)
     self.resolution = self.AREA_W / self.GRID_SIZE
 
     self.robot_sub = rospy.Subscriber("/robot_odom", Odometry, self.robot_callback)
     self.target_sub = rospy.Subscriber("/target/odom", Odometry, self.target_callback)
     self.map_pub = rospy.Publisher("/detections_grid", OccupancyGrid, queue_size=1)
-    self.det_pub = rospy.Publisher("/measurements", Pose, queue_size=1)
+    self.det_pub = rospy.Publisher("/measurements", PoseStamped, queue_size=1)
+    self.times_pub = rospy.Publisher("/times", Int32MultiArray, queue_size=1)
 
     self.map_msg = OccupancyGrid()
     self.map_msg.header.frame_id = "odom"
@@ -89,7 +91,11 @@ class Detector():
     self.map_msg.info.origin.position.y = -0.5*self.AREA_W
     self.map_msg.data = [-1 for _ in range(self.GRID_SIZE**2)]
 
-    self.measurement = Pose()
+    times_msg = Int32MultiArray()
+    times_msg.data = [-1 for _ in range(self.GRID_SIZE**2)]
+    
+    self.times_msg = times_msg
+    self.measurement = PoseStamped()
 
 
 
@@ -98,6 +104,7 @@ class Detector():
     # self.detections = []
 
     self.timer = rospy.Timer(rospy.Duration(self.dt), self.timer_callback)
+    self.t_start = rospy.Time.now()
 
     self.robot = np.zeros(3)
     self.target = np.zeros(2)
@@ -128,19 +135,16 @@ class Detector():
         x_ij = np.array([i*self.resolution - 0.5*self.AREA_W, -0.5*self.AREA_W + j*self.resolution])
         if insideFOV(self.robot, x_ij, self.FOV_DEG, self.ROBOT_RANGE):
           self.map_msg.data[i+j*self.GRID_SIZE] = 0
-    
+          self.times_msg.data[i+j*self.GRID_SIZE] = rospy.Time.now().secs
+      
     if insideFOV(self.robot, self.target, self.FOV_DEG, self.ROBOT_RANGE):
-      # i_t = (self.target[0] + 0.5*self.AREA_W) // self.resolution # + random.randint(-1, 1)
-      # j_t = (self.target[1] + 0.5*self.AREA_W) // self.resolution # + random.randint(-1, 1)
-      # self.map_msg.data[int(i_t+j_t*self.GRID_SIZE)] = 100
-      # self.map_msg.data[int(i_t-1+j_t*self.GRID_SIZE)] = 100
-      # self.map_msg.data[int(i_t+1+j_t*self.GRID_SIZE)] = 100
-      # self.map_msg.data[int(i_t+(j_t-1)*self.GRID_SIZE)] = 100
-      # self.map_msg.data[int(i_t+(j_t+1)*self.GRID_SIZE)] = 100
+      i_t = (self.target[0] + 0.5*self.AREA_W) // self.resolution # + random.randint(-1, 1)
+      j_t = (self.target[1] + 0.5*self.AREA_W) // self.resolution # + random.randint(-1, 1)
+      self.map_msg.data[int(i_t+j_t*self.GRID_SIZE)] = 100
 
-
-      self.measurement.position.x = self.target[0] + -0.5 + np.random.rand()
-      self.measurement.position.y = self.target[1] + -0.5 + np.random.rand()
+      self.measurement.header.stamp = rospy.Time.now()
+      self.measurement.pose.position.x = self.target[0] + -1 + 2*np.random.rand()
+      self.measurement.pose.position.y = self.target[1] + -1 + 2*np.random.rand()
       self.det_pub.publish(self.measurement)
 
 
@@ -148,6 +152,7 @@ class Detector():
 
     self.map_msg.header.stamp = rospy.Time.now()
     self.map_pub.publish(self.map_msg)
+    self.times_pub.publish(self.times_msg)
 
 
 

src/stgp_node.py
@@ -14,9 +14,9 @@ from sklearn.linear_model import LogisticRegression
 
 # ROS stuff
 import rospy
-from geometry_msgs.msg import Point, Twist, Pose
+from geometry_msgs.msg import Point, Twist, Pose, PoseStamped
 from nav_msgs.msg import Odometry, OccupancyGrid
-
+from std_msgs.msg import Float32, Int32MultiArray
 
 
 ### PARAMETERS
@@ -49,6 +49,18 @@ def insideFOV(robot, target, fov, range):
   else:
     return 0
 
+def gauss_pdf(x, y, mean, covariance):
+
+  # points = np.column_stack([x.flatten(), y.flatten()])
+  points = np.array([x, y])
+  points = np.expand_dims(points, 0)
+  # Calculate the multivariate Gaussian probability
+  exponent = -0.5 * np.sum((points - mean) @ np.linalg.inv(covariance) * (points - mean), axis=1)
+  coefficient = 1 / np.sqrt((2 * np.pi) ** 2 * np.linalg.det(covariance))
+  prob = coefficient * np.exp(exponent)
+
+  return prob
+
 def plot_fov(x1, theta, fov_deg, radius, color="tab:blue", ax=None):
   # fig = plt.figure(figsize=(6,6))
   # plt.scatter(neighs[:, 0], neighs[:, 1], marker='*')
@@ -80,17 +92,19 @@ class GPClassifier():
     self.FOV_DEG = rospy.get_param("ROBOT_FOV", 120.0)
     self.FOV_RAD = self.FOV_DEG * math.pi / 180.0
     self.dt = rospy.get_param("dt", 1.0)
-    self.GRID_SIZE = rospy.get_param("GRID_SIZE", 100)
+    self.GRID_SIZE = rospy.get_param("GRID_SIZE", 50)
     self.DETECTIONS_BUFFER = rospy.get_param("DETECTIONS_BUFFER", 200)
     self.resolution = self.AREA_W / self.GRID_SIZE
+    print("Resolution: ", self.resolution)
+    print("Grid size: ", self.GRID_SIZE)
 
-    self.measurements_sub = rospy.Subscriber("/measurements", Pose, self.meas_callback)
+    self.measurements_sub = rospy.Subscriber("/measurements", PoseStamped, self.meas_callback)
     self.measurements = []
     self.detections_subs = [rospy.Subscriber(f"robot{i}/detections_grid", OccupancyGrid, self.detections_callback) for i in range(self.ROBOTS_NUM)]
     self.detections = []
     self.prior_map_pub = rospy.Publisher("/prior_map", OccupancyGrid, queue_size=1)
     self.post_map_pub = rospy.Publisher("/posterior_map", OccupancyGrid, queue_size=1)
-
+    self.times_sub = rospy.Subscriber("/times", Int32MultiArray, self.t_callback)
     
     self.prior_map = OccupancyGrid()
     self.prior_map.header.frame_id = "odom"
@@ -114,11 +128,13 @@ class GPClassifier():
 
 
     self.timer = rospy.Timer(rospy.Duration(self.dt), self.timer_callback)
+    self.T_factor = 0.1
 
     X = np.zeros((self.GRID_SIZE**2, 2))
     self.Y = np.zeros(self.GRID_SIZE**2)
-    self.t_now = rospy.Time.now().secs
-    self.times = self.t_now * np.ones(self.GRID_SIZE**2)
+    self.t_start = rospy.Time.now()
+    self.t_now = (rospy.Time.now() - self.t_start).secs
+    self.times = -np.inf * np.ones(self.GRID_SIZE**2)
 
     for i in range(0, self.GRID_SIZE**2, self.GRID_SIZE):
       X[i:i+self.GRID_SIZE, 0] = self.AREA_W*i/(self.GRID_SIZE**2)
@@ -138,14 +154,23 @@ class GPClassifier():
     det_ids = [i for i, v in enumerate(msg.data) if v != -1]
     for i in det_ids:
       self.prior_map.data[i] = msg.data[i]
+      # self.times[i] = (msg.header.stamp - self.t_start).secs
 
   
   def meas_callback(self, msg):
-    x = msg.position.x
-    y = msg.position.y
-    self.measurements.append(np.array([x,y]))
+    x = msg.pose.position.x
+    y = msg.pose.position.y
+    time = msg.header.stamp
+    self.measurements.append(np.array([x, y, time]))
     if len(self.measurements) > self.DETECTIONS_BUFFER:
       self.measurements.pop(0)
+
+
+  def t_callback(self, msg):
+    det_ids = [i for i, v in enumerate(msg.data) if v != -1]
+    new_ts = []
+    for i in det_ids:
+      self.times[i] = max(msg.data[i], self.times[i])     # keep only the most recent
     
     
 
@@ -154,14 +179,14 @@ class GPClassifier():
   def timer_callback(self, e):
     
     timerstart = time.time()
-    self.t_now = rospy.Time.now().secs
+    self.t_now = (rospy.Time.now() - self.t_start).secs
 
     # out = [[i,v] for i,v in enumerate(self.prior_map.data) if v != -1]
     # det_ids = [out[i][0] for i in range(len(out))]
     # values = [out[i][1]/100 for i in range(len(out))]
     det_ids = [i for i,v in enumerate(self.prior_map.data) if v != -1]
     for i in det_ids:
-      self.times[i] = self.t_now
+      # self.times[i] = self.t_now
       if i not in self.det_ids:
         self.det_ids.append(i)
     
@@ -170,20 +195,30 @@ class GPClassifier():
 
     print("Number of measurements: ", len(self.measurements))
     
+    
     for meas in self.measurements:
       i_t = (meas[0] + 0.5*self.AREA_W) // self.resolution # + random.randint(-1, 1)
       j_t = (meas[1] + 0.5*self.AREA_W) // self.resolution # + random.randint(-1, 1)
       self.Y[int(i_t+j_t*self.GRID_SIZE)] = 1
+      # self.times[int(i_t+j_t*self.GRID_SIZE)] = (meas[2]).secs
       # self.post_map.data[int(i_t+j_t*self.GRID_SIZE)] = 100
     
+    
+    """
+    for i in range(self.GRID_SIZE):
+      for j in range(self.GRID_SIZE):
+        x_ij = np.array([i*self.resolution - 0.5*self.AREA_W, -0.5*self.AREA_W + j*self.resolution])
+        self.Y[i+j*GRID_SIZE] = gauss_pdf(x_ij[0], x_ij[1], self.measurements[-1], 2.0)
+    """
     
     # self.Y[det_ids] = values
 
     X_train = self.X[self.det_ids]
     y_train = self.Y[self.det_ids]
-    t_train = self.times[self.det_ids]
+    t_train = self.T_factor*(self.times[self.det_ids] - self.t_start.secs)
+    print("times max: ", np.max(t_train))
+    # print("t_train: ", t_train)
     Xst_train = np.concatenate((X_train, np.expand_dims(t_train, 1)), 1)
-    print("Xst shape: ", Xst_train.shape)
 
     if 1 not in y_train:
       y_train[np.random.randint(len(y_train))] = 1
@@ -192,7 +227,8 @@ class GPClassifier():
     self.gp.fit(Xst_train, y_train)
 
     # Prediction
-    times_now = self.t_now * np.ones((self.X.shape[0], 1))
+    times_now = self.T_factor * self.t_now * np.ones((self.X.shape[0], 1))
+    print("Time now: ", self.t_now)
     X_st = np.concatenate((self.X, times_now), 1)
     y_pred = self.gp.predict(X_st)
     y_prob = self.gp.predict_proba(X_st)